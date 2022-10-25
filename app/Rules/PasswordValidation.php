f9d7266a494672b5ebc98cebd107ac66bb9804fb
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-10-26 00:10:21 +0330

    define rule in password validation

--- app/Rules/PasswordValidation.php
+++ app/Rules/PasswordValidation.php
@@ -3,19 +3,14 @@
 namespace App\Rules;
 
 use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Str;
 
 class PasswordValidation implements Rule
 {
-    /**
-     * Create a new rule instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
 
+    public $lengthPasses = true;
+
+    public $specialCharPasses = true;
     /**
      * Determine if the validation rule passes.
      *
@@ -25,7 +20,9 @@ class PasswordValidation implements Rule
      */
     public function passes($attribute, $value)
     {
-        //
+        $this->lengthPasses = (Str::length($value) >= 8);
+        $this->specialCharPasses = ((bool) preg_match('/[^A-Za-z0-9]/', $value));
+        return ($this->lengthPasses && $this->specialCharPasses);
     }
 
     /**
@@ -35,6 +32,18 @@ class PasswordValidation implements Rule
      */
     public function message()
     {
-        return 'The validation error message.';
+        switch (true) {
+            case ! $this->lengthPasses
+                && $this->specialCharPasses:
+                return 'Password must be at least 8 Characters.';
+            case !$this->specialCharPasses
+                && $this->lengthPasses:
+                return 'Password must be at least one special character.';
+            case !$this->lengthPasses
+                && !$this->specialCharPasses:
+                return 'Password must be at least 8 Characters and contain at least one special Character.';
+            default:
+                return 'Password must be at least 8 Characters';
+        }
     }
 }
