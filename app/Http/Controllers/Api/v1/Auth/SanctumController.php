489ec6a23327ad9483daefb23b1931015a4e63e7
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-10-26 02:18:00 +0330

    define user resource

--- app/Http/Controllers/Api/v1/Auth/SanctumController.php
+++ app/Http/Controllers/Api/v1/Auth/SanctumController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers\Api\v1\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use http\Exception\InvalidArgumentException;
 use Illuminate\Http\Request;
 
 class SanctumController extends Controller
 {
-    public function register()
+    public function register(RegisterRequest $request)
     {
-
+        try {
+            $attributes = User::create($request->validated());
+            return response()->json('user created success.');
+        } catch (\InvalidArgumentException $e) {
+            return response()->json($e->getMessage());
+        }
     }
 
     public function login()
