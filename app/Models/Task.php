ac40aeccfbea314ddf734d2e03354452dc2405f4
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-10-25 23:33:21 +0330

    Add Method to Task Model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Task extends Model
 {
     use HasFactory;
+
+    protected $with = ['children'];
+    protected $fillable = [
+        'title', 'description', 'is_completed', 'due_at', 'parent_id'
+    ];
+
+    public function children(): HasMany
+    {
+        return $this->hasMany(Task::class, 'parent_id');
+    }
 }
