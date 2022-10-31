3691645ef80584265ecfda7c249e663216655392
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-11-01 03:04:35 +0330

    test login

--- tests/Feature/Api/v1/Auth/SanctumTest.php
+++ tests/Feature/Api/v1/Auth/SanctumTest.php
@@ -5,11 +5,11 @@ namespace Tests\Feature\Api\v1\Auth;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class SanctumTest extends TestCase
 {
-    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
@@ -25,11 +25,11 @@ class SanctumTest extends TestCase
     public function test_new_user_can_register()
     {
         $this->postJson(route('v1.auth.register'),[
-            'first_name' => 'jack',
-            'last_name' => 'jackson',
-            'email' => 'jackson@gmail.com',
-            'password' => 'jack6070!A',
-            'password_confirmation' => 'jack6070!A'
+            'first_name' => 'sam',
+            'last_name' => 'samson',
+            'email' => 'sam@gmail.com',
+            'password' => 'Jack211!!!',
+            'password_confirmation' => 'Jack211!!!'
         ])->assertCreated();
         $this->assertDatabaseHas('users', ['first_name' => 'jack']);
     }
@@ -37,8 +37,8 @@ class SanctumTest extends TestCase
     public function test_user_can_login()
     {
         $response = $this->postJson(route('v1.auth.login'), [
-            'email' => 'jackson@gmail.com',
-            'password' => 'jack6070!A',
+            'email' => 'sam@gmail.com',
+            'password' => '$2y$10$FgGS3pQDO0vM5goqJs1UQ.2aAvTZwej1XxJplagmsKVGQyAekJa/S',
             'device_name' => 'apple'
         ])->assertOk();
         $this->assertArrayHasKey('token', $response->json());
