92cd9e90af824337b612da3eff6ef1ab70f40457
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-11-06 18:29:37 +0330

    fix bug resource

--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -15,7 +15,7 @@ class TaskResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' = $this->id,
+            'id' => $this->id,
             'title' => $this->title,
             'description' => $this->description,
             'author' => $this->user_id,
