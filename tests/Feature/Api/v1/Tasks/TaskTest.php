5a9a5fe7a762712ffb9568040b3e0865ee0ca707
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-11-06 22:25:13 +0330

    define new test

--- tests/Feature/Api/v1/Tasks/TaskTest.php
+++ tests/Feature/Api/v1/Tasks/TaskTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Api\v1\Tasks;
 
+use App\Models\Task;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -9,4 +10,19 @@ use Tests\TestCase;
 class TaskTest extends TestCase
 {
 
+    public function test_user_can_store_task()
+    {
+        $task = Task::factory()->create();
+        $this->postJson(route('v1.task.store'),[
+            'title' => $task->title,
+            'description' => $task->description,
+            'due_at' => $task->due_at
+        ])->assertCreated();
+
+        $this->assertDatabaseHas('tasks', [
+            'title' => $task->title,
+            'description' => $task->description,
+            'due_at' => $task->due_at
+        ]);
+    }
 }
