3588c0fa5db5ec05ffef79079a15474ab3ae51af
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-11-06 22:02:34 +0330

    add task test

---
+++ tests/Feature/Api/v1/Tasks/TaskTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Feature\Api\v1\Tasks;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TaskTest extends TestCase
+{
+
+}
